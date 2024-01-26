59f77596bfb9531bf1bc3f38f18f2b7bc429e5dd
Dharshan S <89525251+Dharshan15@users.noreply.github.com>
2024-01-26 13:09:32 +0530

    Add files via upload

---
+++ AI_sotry_teller/face.py
@@ -0,0 +1,20 @@
+# emotion_capture.py
+import asyncio
+import cv2
+from hume import HumeStreamClient, StreamSocket
+from hume.models.config import FaceConfig
+
+async def capture_and_save_image(file_path):
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cv2.imwrite(file_path, frame)
+    cap.release()
+
+async def capture_emotion():
+    await capture_and_save_image("./extra/emotion.jpg")
+    client = HumeStreamClient("AVGYpkETOCN8Zw9NQoV0ePFz3K4O6BGdVpkFEhao342dddZK")
+    config = FaceConfig(identify_faces=True)
+    
+    async with client.connect([config]) as socket:
+        result = await socket.send_file("./extra/emotion.jpg")
+        return result
